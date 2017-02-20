c14d80637c3cf0195ae3be7baf488c8484c5dd41
Dwi Purnomo <dwipurnomo.yk@gmail.com>
2017-02-20 11:59:27 +0700

    Add dummy data
    i will try to insert and add dummy data with Faker libray

--- __init__.py
+++ __init__.py
@@ -1,10 +1,40 @@
-import pprint
+import datetime
 from openedoo.core.libs import blueprint
-from openedoo.core.db import Query
+from openedoo import db
+from database import User
+from flask import jsonify
+from faker import Faker
 
-module_employee = blueprint('employee', __name__)
+fake = Faker()
+
+module_employee = blueprint('module_employee', __name__)
 
 
-@module_employee.route('/', methods=['POST', 'GET'])
+@module_employee.route('/insert', methods=['GET'])
 def index():
-    return 'ko'
+    try:
+        data = {
+            'username': fake.user_name(),
+            'password': fake.password(),
+            'fullname': fake.name(),
+            'access_token': 'glakhgaie837w9',
+            'public_key': 'asdalksjd',
+            'private_key': 'asde',
+            'status': 0,
+            'role': 't',
+            'created': datetime.datetime.now(),
+            'last_login': datetime.datetime.now(),
+            'nip': fake.random_digit_not_null()
+        }
+        paimin = User(data)
+        db.session.add(paimin)
+        db.session.commit()
+        return 'ko'
+    except Exception as e:
+        message = {'error': str(e)}
+        return jsonify(message)
+
+
+@module_employee.route('/employees', methods=['GET'])
+def employees():
+    return jsonify([i.serialize for i in User.query.all()])

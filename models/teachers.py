0343a79259c732b4ab80fb5f148f553439a4e3eb
dwi purnomo <dwipurnomo.yk@gmail.com>
2017-03-15 21:25:24 +0700

    Fix wrong foreign key

--- models/teachers.py
+++ models/teachers.py
@@ -1,5 +1,5 @@
-from openedoo import db
-from openedoo import config
+from openedoo_project import db
+from openedoo_project import config
 from .users import User
 from .subjects import Subject
 
@@ -9,8 +9,5 @@ database_prefix = config.database_prefix
 class Teacher(db.Model):
     __tablename__ = '{db_prefix}_teacher'.format(db_prefix=database_prefix)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer,
-                        db.ForeignKey(User.id, ondelete='CASCADE'),
-                        nullable=False)
-    subject_id = db.Column(db.Integer,
-                           db.ForeignKey(Subject.id, ondelete='CASCADE'))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    subject_id = db.Column(db.Integer, db.ForeignKey(Subject.id))

2201a23aa0407496402f0766d09f5df9951b9709
dwi purnomo <dwipurnomo.yk@gmail.com>
2017-04-11 23:24:39 +0700

    Change bad function decorator implementation

--- models/employees.py
+++ models/employees.py
@@ -6,7 +6,7 @@ from .users import User
 
 class Employee(User):
 
-    @staticmethod
+    @classmethod
     def is_exist(self, username):
         employee = self.query.get(username=username).first()
         return employee
